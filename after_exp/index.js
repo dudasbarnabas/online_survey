cb2ea843a6f9cd627bbb69f0108690b344501f8b
dudasbarnabas <102540665+dudasbarnabas@users.noreply.github.com>
2025-10-28 10:29:34 +0100

    Update index.js

--- after_exp/index.js
+++ after_exp/index.js
@@ -57,18 +57,25 @@ pages.push({
   elements: [{
         "type": "expression",
          "name": "Kedves Résztvevő! \n\nA kérdőívben különböző képeket mutatunk be. A bemutatott képeket kérjük értékelje a valencia (mennyire kellemes vagy kellemetlen az adott kép), arousal (mennyire intenzív érzelmeket vált ki az adott kép) és realisztikusság (mennyire találja realisztikusnak az adott képet) mentén egy-egy kilencfokú skálán, ahol:  \n\nValencia: \n1=kellemetlen/negatív  \n5=semleges  \n9=kellemes/pozitív  \n\nArousal:  \n1=nyugodt/békés  \n5=semleges  \n9=izgatott/feszült  \n\nRealisztikusság  \n1= Egyáltalán nem reális  \n9= Reális  \n\nHa bármi kérdése lenne kérem keresse a vizsgálatvezetőt!"
-  }]
+  },{
+        type: "text",
+        name: "id",
+        title: "Kérem adja meg az anonimizált kódját!",
+        isRequired: true
+    }
+  ]
 });
 
 pages.push({
     name: "demography",
     elements: [
+    // {
+    //     type: "text",
+    //     name: "id",
+    //     title: "Kérem adja meg az anonimizált kódját!",
+    //     isRequired: true
+    // }, 
     {
-        type: "text",
-        name: "id",
-        title: "Kérem adja meg az anonimizált kódját!",
-        isRequired: true
-    }, {
         type: "text",
         name: "kor",
         title: "Kérem adja meg életkorát!",
